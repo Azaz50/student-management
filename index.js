193d7b3ac81c56b9e47554aaa4462831b0b5dfc3
Azaz Mohammad <azazmohammad14@gmail.com>
2025-12-17 16:09:32 +0530

    rate limit and helmet apply

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ const mongoose = require('mongoose');
 const { MulterError } = require('multer');
 const auth = require('./middleware/auth.js'); // for jwt authentication
 const userRoutes = require('./routes/users.route.js'); // for jwt authentication
-const cors = require('cors')
+const cors = require('cors');
 const path = require('path')
-
-
-const studentRoutes = require('./routes/students.route.js');
-
+const rateLimit = require('express-rate-limit'); // rate limit to protect your site from attacker to send max request at a time
+const helmet = require('helmet'); // helmet to restrict your site to see your backend or frontend technology
 const connectDB = require('./config/db.js');
+const studentRoutes = require('./routes/students.route.js');
 require('dotenv').config();
 
-// Database Connection
-// connectDB(); // Removed from here
 
 const ensureDbConnected = async (req, res, next) => {
   try {
@@ -30,24 +27,40 @@ app.use(cors({
   origin: "*"
 }));
 
+// Middleware to parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: false }));
+// Middleware to parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" }
+  })
+);
+
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: "Too many requests from this IP, please try again after a minute"
+});
+
+app.use(limiter);
+
 
 app.use('/uploads', express.static(path.join(__dirname,'uploads'), {
     setHeaders: (res, path, stat) => {
         res.set('Cross-Origin-Resource-Policy', 'cross-origin');
     }
 }))
-// Middleware to parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({ extended: false }));
-// Middleware to parse JSON bodies (as sent by API clients)
-app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Student Management API!');
 });
 
-// API routes for students
-// All routes defined in studentRoutes will be prefixed with /api/students
-// API routes for users
 // All routes defined in userRoutes will be prefixed with /api/users
 app.use('/api/users', ensureDbConnected, userRoutes)
 app.use('/api/students', ensureDbConnected, auth, studentRoutes);
