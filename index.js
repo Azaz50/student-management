5148f1b242e5ec926677921d61c490fe8c81affa
Azaz Mohammad <azazmohammad14@gmail.com>
2025-12-17 16:04:19 +0530

    rate limit and helmet apply

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const mongoose = require('mongoose');
 const { MulterError } = require('multer');
 const auth = require('./middleware/auth.js'); // for jwt authentication
 const userRoutes = require('./routes/users.route.js'); // for jwt authentication
-const cors = require('cors');
+const cors = require('cors')
 const path = require('path')
-// const rateLimit = require('express-rate-limit'); // rate limit to protect your site from attacker to send max request at a time
-// const helmet = require('helmet'); // helmet to restrict your site to see your backend or frontend technology
-const connectDB = require('./config/db.js');
+
+
 const studentRoutes = require('./routes/students.route.js');
+
+const connectDB = require('./config/db.js');
 require('dotenv').config();
 
+// Database Connection
+// connectDB(); // Removed from here
 
 const ensureDbConnected = async (req, res, next) => {
   try {
@@ -23,28 +26,9 @@ const ensureDbConnected = async (req, res, next) => {
   }
 };
 
-app.use(cors());
-
-app.options('*', cors());
-// Middleware to parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({ extended: false }));
-// Middleware to parse JSON bodies (as sent by API clients)
-app.use(express.json());
-
-// app.use(
-//   helmet({
-//     crossOriginResourcePolicy: { policy: "cross-origin" }
-//   })
-// );
-
-
-// const limiter = rateLimit({
-//   windowMs: 60 * 1000,
-//   max: 100,
-//   skip: (req) => req.method === 'OPTIONS'
-// });
-
-// app.use(limiter);
+app.use(cors({
+  origin: "*"
+}));
 
 
 app.use('/uploads', express.static(path.join(__dirname,'uploads'), {
@@ -52,12 +36,18 @@ app.use('/uploads', express.static(path.join(__dirname,'uploads'), {
         res.set('Cross-Origin-Resource-Policy', 'cross-origin');
     }
 }))
-
+// Middleware to parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: false }));
+// Middleware to parse JSON bodies (as sent by API clients)
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Student Management API!');
 });
 
+// API routes for students
+// All routes defined in studentRoutes will be prefixed with /api/students
+// API routes for users
 // All routes defined in userRoutes will be prefixed with /api/users
 app.use('/api/users', ensureDbConnected, userRoutes)
 app.use('/api/students', ensureDbConnected, auth, studentRoutes);
